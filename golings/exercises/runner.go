b0af5d74a54d098392e6e25c1e908052d9d15915
MrSloth-dev <joao.a.barbosa@proton.me>
2026-06-04 02:39:13 +0100

    refactor runner.go to be able to handle go run and go test

--- golings/exercises/runner.go
+++ golings/exercises/runner.go
@@ -3,7 +3,10 @@ package exercises
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
+
 )
 
 type Result struct {
@@ -13,25 +16,72 @@ type Result struct {
 }
 
 func (e Exercise) Run() (Result, error) {
-	args := BuildArgs(e)
-	cmd := exec.Command("go", args...)
+	if e.Mode == "compile" {
+		return runCompile(e)
+	}
+	return runTest(e)
+}
+func runCompile(e Exercise) (Result, error) {
+	cmd := exec.Command("go", "run", "-tags=golings", fmt.Sprintf("./%s", e.Path))
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
-
 	err := cmd.Run()
-
 	return Result{Exercise: e, Out: stdout.String(), Err: stderr.String()}, err
 }
 
-func BuildArgs(e Exercise) []string {
-	args := []string{}
-	if e.Mode == "compile" {
-		args = append(args, "run")
-	} else {
-		args = append(args, "test", "-v", "-race")
+func runTest(e Exercise) (Result, error) {
+	src, err := os.ReadFile(e.Path)
+	if err != nil {
+		return Result{Exercise: e, Err: err.Error()}, err
 	}
 
-	args = append(args, fmt.Sprintf("./%s", e.Path))
-	return args
+	tmp, err := os.MkdirTemp("", "golings-*")
+	if err != nil {
+		return Result{Exercise: e, Err: err.Error()}, err
+	}
+	defer os.RemoveAll(tmp)
+	if err := os.WriteFile(filepath.Join(tmp, "main.go"),
+		[]byte("package main\nfunc main(){}\n"), 0644); err != nil {
+		return Result{Exercise: e, Err: err.Error()}, err
+	}
+
+	if err := os.WriteFile(filepath.Join(tmp, "go.mod"),
+							[]byte("module golings_exercise\n\ngo 1.26\n"),
+							0644); err != nil {
+			return Result{Exercise: e, Err: err.Error()}, err
+		}
+
+	dest := filepath.Join(tmp, "main_test.go")
+	if err := os.WriteFile(dest, src, 0644); err != nil {
+		return Result{Exercise: e, Err: err.Error()}, err
+	}
+	cmd := exec.Command("go", "test", "-v", "-race", "-tags=golings")
+	cmd.Dir = tmp
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	return Result{Exercise: e, Out: stdout.String(), Err: stderr.String()}, err
+
 }
+
+// func (e Exercise) Run() (Result, error) {
+// 	args := BuildArgs(e)
+// 	cmd := exec.Command("go", args...)
+// 	var stdout, stderr bytes.Buffer
+// 	cmd.Stdout = &stdout
+// 	cmd.Stderr = &stderr
+//
+// 	err := cmd.Run()
+//
+// 	return Result{Exercise: e, Out: stdout.String(), Err: stderr.String()}, err
+// }
+
+func BuildArgs(e Exercise) []string {
+	if e.Mode == "compile" {
+		return []string{"run", "-tags=golings", fmt.Sprintf("./%s", e.Path)}
+	}
+	return []string{"test", "-v", "-race", "-tags=golings", fmt.Sprintf("./%s", e.Path)}
+}
+
